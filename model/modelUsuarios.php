c4593716a0f7c56cce6b438ca3dd2b1ec37368d3
Camila Andrade <camila.rda@outlook.com>
2024-06-20 20:43:33 -0300

    inclusão controller

---
+++ model/modelUsuarios.php
@@ -0,0 +1,61 @@
+<?php
+
+class modelUsuarios {
+    public function listarUsuarios(){
+        try{
+            $pdo = Database::conexao();
+            $sql =$pdo->query("SELECT * FROM usuario");
+            $resultado = $sql->fetchAll(PDO::FETCH_ASSOC);
+            return $resultado;
+        }catch(PDOException $e){
+            return false;
+    
+        }
+    }
+
+    public function cadastrarUsuario($nome,$email,$senha){
+        try{
+            $pdo = Database::conexao();
+            $sql = $pdo->prepare("INSERT INTO usuario (nome,email,senha) VALUES (:nome,:email, :senha)");
+            $sql->bindParam(":nome",$nome);
+            $sql->bindParam(":email",$email);
+            $sql->bindParam(":senha",$senha);
+            $sql->execute();
+            return true;
+
+        }catch(PDOException $e){
+            return false;
+        }
+    }
+
+    public function atualizarUsuario($nome,$email,$senha){
+        try{
+            $pdo = Database::conexao();
+            $prepare = $pdo->prepare("UPDATE usuario SET nome = :nome, email = :email, senha =:senha");
+            $prepare->bindParam("nome", $nome);
+            $prepare->bindParam("email", $email);
+            $prepare->bindParam("senha",$senha);
+            $prepare->execute();
+            return true;
+        } catch (PDOException $e){
+            return false;
+        }
+    }
+
+    public function excluirUsuario($id_usuario){
+        try{
+            $pdo = Database::conexao();
+            $excluir = $pdo->prepare("DELETE FROM usuario WHERE id_usuario = :id_usuario");
+            $excluir->bindParam("id_usuario", $id_usuario);
+            $excluir->execute();
+            return true;
+
+        }catch(PDOException $e){
+            return false;
+        }
+    }
+}
+
+
+
+?>
